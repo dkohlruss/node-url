d2198bfda6ea95235ca7364880f5bf14c81f49b8
dkohlruss <dkohlruss@gmail.com>
2017-03-13 10:29:43 -0600

    Setupd app for heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-process.env.MONGODB_URI = 'mongodb://localhost:27017/UrlShortener';
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UrlShortener');
 
 module.exports = {
   mongoose
